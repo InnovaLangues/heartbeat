dd7235244ff8800f627720050ce0545b5c42a4b6
Donovan Tengblad <purplefish32@gmail.com>
2015-04-25 16:26:57 +0200

    Added Toast class

---
+++ src/Innova/Heartbeat/AppBundle/Common/Toast.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Innova\Heartbeat\AppBundle\Common;
+
+/**
+ * Toast object (notifications)
+ */
+class Toast
+{
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Toast
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Toast
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Toast
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
