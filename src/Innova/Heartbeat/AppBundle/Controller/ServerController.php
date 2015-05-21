818b9c8e54effe7b63ea58a2f27403a2d9b54105
Donovan Tengblad <purplefish32@gmail.com>
2015-05-21 07:55:46 +0200

    Test

--- src/Innova/Heartbeat/AppBundle/Controller/ServerController.php
+++ src/Innova/Heartbeat/AppBundle/Controller/ServerController.php
@@ -50,12 +50,9 @@ class ServerController extends Controller
         $date    = null;
 
         $serverDatas = $this->get('innova.serverdata.manager')->findByServerId($server->getUid(), 1000); //1440 24h
-        
-        print_r($serverDatas);
-        die();
 
         $serverData  = $serverDatas[0];
-        //$serverDatas = $this->container->get('serializer')->serialize($serverDatas, 'json');
+        $serverDatas = $this->container->get('serializer')->serialize($serverDatas, 'json');
 
         if ($serverData) {
             $details = $serverData->getDetails();
@@ -67,7 +64,7 @@ class ServerController extends Controller
                 'server' => $server,
                 'details' => $details,
                 'channels' => array($server->getUid()),
-                'serverDatas' => json_encode($serverDatas->toArray()),
+                'serverDatas' => $serverDatas->toArray(),
             )
         );
     }
