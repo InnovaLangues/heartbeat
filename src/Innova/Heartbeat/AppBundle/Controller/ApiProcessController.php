e5945aa6fd51229eaaa393e741c8466b0b41d5bf
Donovan Tengblad <purplefish32@gmail.com>
2015-06-09 16:42:36 +0200

    Removed unused use

--- src/Innova/Heartbeat/AppBundle/Controller/ApiProcessController.php
+++ src/Innova/Heartbeat/AppBundle/Controller/ApiProcessController.php
@@ -9,9 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Innova\Heartbeat\AppBundle\Entity\Server;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Innova\Heartbeat\AppBundle\Document\Snapshot;
-use Innova\Heartbeat\AppBundle\Document\Process;
 
 /**
  * Process controller
